856917c3b22a3a94dcda6fe9e175bd6d02fafee2
Samson-64 <148760141+Samson-64@users.noreply.github.com>
2026-09-01 12:18:06 +0300

    add database connection

--- app/database.py
+++ app/database.py
@@ -5,10 +5,10 @@ from app.config import get_settings
 settings = get_settings()
 
 engine = create_engine(
-    settings.DATABASE_URL,
+    settings.database_url,
     pool_pre_ping=True,
-    pool_size=10,
-    max_overflow=20,
+    pool_size=3,
+    max_overflow=1,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
